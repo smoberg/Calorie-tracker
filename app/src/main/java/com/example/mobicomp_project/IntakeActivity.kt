3aa08b9fb3ddf599a3f54b824d298946c195614a
Santeri Moberg <santeri.moberg30@gmail.com>
2020-03-18 11:37:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobicomp_project/IntakeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobicomp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntakeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intake)
+    }
+}
