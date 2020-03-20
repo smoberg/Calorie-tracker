69057a10e815fc2d2e52e7d0821dfc23d777d0d9
Santeri Moberg <santeri.moberg30@gmail.com>
2020-03-20 13:39:28 +0200

    added fab to move to intake activity. Started working on main activity layout.

--- app/src/main/java/com/example/mobicomp_project/MainActivity.kt
+++ app/src/main/java/com/example/mobicomp_project/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.mobicomp_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calories_text.setText("1234")
+
+
+        fab.setOnClickListener{
+            startActivity(Intent(applicationContext, IntakeActivity::class.java))
+
+        }
+
     }
 }
